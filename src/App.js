02c2914e6388e3a198e1be2037d9c649e3e57c58
gorstop <gorstop@gmail.com>
2018-05-09 21:44:20 -0700

    register request sent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from './Header';
 import './App.css';
+import { Main } from './Main';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Main/>
       </div>
     );
   }
