98409069ecd83afb113850f43fb687fee63d6c01
gorstop <gorstop@gmail.com>
2018-05-09 20:06:09 -0700

    set top bar

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ export class Header extends React.Component {
     return (
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Gossan</h1>
         </header>
     );
   }
