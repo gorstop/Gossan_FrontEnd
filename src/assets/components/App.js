72d621cfda6a3b6b5b45790f9b54330e94766e33
gorstop <gorstop@gmail.com>
2018-05-16 08:31:32 -0700

    login page added

--- src/assets/components/App.js
+++ src/assets/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from './Header';
-import './App.css';
+import '../../styles/App.css';
 import { Main } from './Main';
 
 class App extends React.Component {
