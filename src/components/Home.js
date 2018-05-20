985e826f5c489f8e33d08edeba92e2a3bacb3ea9
gorstop <gorstop@gmail.com>
2018-05-20 16:25:46 -0700

    error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { GEO_OPTIONS } from '../constants';
+import { Tabs, Button, Spin } from 'antd';
+import $ from 'jquery';
+import {API_ROOT, GEO_OPTIONS, POS_KEY, TOKEN_KEY, AUTH_PREFIX} from '../constants';
+
+
+const TabPane = Tabs.TabPane;
+
 
 export class Home extends React.Component {
+  state = {
+    loadingGeoLocation: false,
+    loadingPosts: false,
+    error: '',
+  }
+
   componentDidMount() {
     this.getGeoLocation()
   }
 
   getGeoLocation = () => {
+    this.setState({ loadingGeoLocation: true, error: ''});
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
         this.onSuccessLoadGeoLocation,
@@ -14,21 +27,70 @@ export class Home extends React.Component {
         GEO_OPTIONS,
         );
     } else {
-
+      this.setState({ error: 'Your browser does not support geolocation'});
     }
   }
 
   onSuccessLoadGeoLocation = (position) => {
+    this.setState({ loadingGeoLocation: true, error: ''});
+    this.setState({ loadingGeoLocation: false});
     console.log(position);
+    const { latitude, longitude} = position.coords;
+    localStorage.setItem('POS_KEY', JSON.stringify({ lat: latitude, lon: longitude}));
+    this.loadNearByPosts();
   }
 
   onFailedLoadGeoLocation = (error) => {
+    this.setState({ loadingGeoLocation: false});
     console.log(error);
+    this.setState({ error: 'Failed to load geolocation'});
   }
 
+  loadNearByPosts = () => {
+    this.setState({ loadingPosts : true, error: '' });
+    const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+    $.ajax({
+      url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20000`,
+      method: 'GET',
+      headers: {
+        Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
+      },
+    }).then((response) => {
+      console.log(response);
+      this.setState({ loadingPosts : false, error: ''});
+    }, (response) => {
+      console.log(response.responseText);
+      this.setState({ loadingPosts : false });
+      this.setState({ error: 'Failed to load posts'});
+    }).catch((error) => {
+      console.log(error);
+    });
+
+  }
+
+  getGalleryPanelContent = () => {
+    if(this.state.error) {
+      return <div>{this.state.error}</div>
+    } else if (this.state.loadingGeoLocation) {
+      return <Spin tip="loading geo location..."/>
+    } else if (this.state.loadingPosts) {
+      return <Spin tip="loading posts..."/>
+    } else {
+      return '';
+    }
+  }
   render() {
+    const operations = <Button type="primary">Create New Post</Button>;
+
     return (
-      <div>This is home</div>
+      <div className="main-tabs">
+        <Tabs tabBarExtraContent={operations}>
+          <TabPane tab="Posts" key="1">
+            {this.getGalleryPanelContent()}
+            </TabPane>
+          <TabPane tab="Map" key="2">Map</TabPane>
+        </Tabs>
+      </div>
     );
   }
 }
