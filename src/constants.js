ea899fcd72eea9ce89a36d5f5263ac1c88c493c4
gorstop <gorstop@gmail.com>
2018-06-01 12:27:52 -0700

    final version

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
+export const API_ROOT = 'https://curious-setup-199302.appspot.com';
 export const TOKEN_KEY = 'TOKEN_KEY';
 export const POS_KEY = 'POS_KEY';
 export const AUTH_PREFIX = 'Bearer';
